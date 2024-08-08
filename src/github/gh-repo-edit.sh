1ff819e5d58598f76d3d9ea4bea878c18d0c849a
Rodrigo Motta <rmotta.net@gmail.com>
2024-08-08 12:31:30 -0300

    Basic features on Github with the following:
    commit 22627c72a1687e2d8ca990dfd2078fd41269f647
    Author: Rodrigo Motta <rmotta.net@gmail.com>
    Date:   Thu Aug 8 12:27:46 2024 -0300
    
        Github repo edition
    
    commit a1fda37739ef9de40baf2eb52c90c5cb41a237a0
    Author: Rodrigo Motta <rmotta.net@gmail.com>
    Date:   Thu Aug 8 12:27:46 2024 -0300
    
        Github repo deletion
    
    commit 713fd5f18f1266971412e063eb3fdeb0b14b28eb
    Author: Rodrigo Motta <rmotta.net@gmail.com>
    Date:   Thu Aug 8 12:27:46 2024 -0300
    
        Github repo creation

---
+++ src/github/gh-repo-edit.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+
+
+function edit_github_repo() {
+    local REPO
+    local OWNER
+    local new_description
+    local payload
+    local endpoint
+    local response
+
+    # Get the new description as an argument
+    new_description="$1"
+
+    # Checks if the parameter was provided
+    if [[ -z "$new_description" ]]; then
+        echo "A new description is required."
+        exit 1
+    fi
+
+    # Name of repository
+    REPO="$2"
+    
+    # Checks if the parameter was provided or use the name of the current directory
+    if [[ -z "$REPO" ]]; then
+        REPO=$(basename "$PWD")
+    fi
+    
+    # Repo owner
+    OWNER="$3"
+    
+    # Checks if the parameter was provided or use the default
+    if [[ -z "$OWNER" ]]; then
+        OWNER="$DEF_GH_OWNER"
+    fi
+
+    # Create the JSON payload for the repository
+    payload='{"description": "'"$new_description"'"}'
+    
+    # Construct API Endpoint
+    endpoint="https://api.github.com/repos/$OWNER/$REPO"
+    
+    # Edit the repository using curl
+    response=$(curl -sSL -X PATCH "$endpoint" \
+        -H "Accept: application/vnd.github+json" \
+        -H "Authorization: Bearer $GITHUB_TOKEN" \
+        -H "X-GitHub-Api-Version: 2022-11-28" \
+        -d "$payload" )
+    
+    # Check if there are erros
+    echo "$response" | jq -r '.message'
+}
+
+
+edit_github_repo "$1" "$2" "$3"
