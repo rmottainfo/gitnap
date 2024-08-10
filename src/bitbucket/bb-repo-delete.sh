c1f83298867fd702f07c724fceb94b1326ea8460
Rodrigo Motta <rmotta.net@gmail.com>
2024-08-10 15:08:22 -0300

    Basic features on Bitbucket with the following:
    commit cba5ec83645fe2affa89f8af1ff8c5a0c9e4c0e6
    Author: Rodrigo Motta <rmotta.net@gmail.com>
    Date:   Sat Aug 10 15:08:22 2024 -0300
    
        Bitbucket repo edition
    
    commit a681a40bdf6174258898145af71b1b4c765124e0
    Author: Rodrigo Motta <rmotta.net@gmail.com>
    Date:   Sat Aug 10 15:08:22 2024 -0300
    
        Bitbucket repo deletion
    
    commit 8aad7b83332bfdd7c7809ba041c5c93c2fa11b01
    Author: Rodrigo Motta <rmotta.net@gmail.com>
    Date:   Sat Aug 10 15:08:22 2024 -0300
    
        Bitbucket repo creation

---
+++ src/bitbucket/bb-repo-delete.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+
+# Construct basic authentication header
+BITBUCKET_AUTH="Authorization: Basic $(echo -n "$BITBUCKET_USER:$BITBUCKET_APPWD" | base64)"
+
+
+function delete_bitbucket_repo() {
+    local REPO
+    local WORKSPACE
+    local endpoint
+    local response
+    
+    # Name of repository
+    REPO="$1"
+    
+    # Checks if the parameter was provided or use the name of the current directory
+    if [[ -z "$REPO" ]]; then
+        REPO=$(basename "$PWD")
+    fi
+
+    # Name of workspace
+    WORKSPACE="$2"
+    
+    # Checks if the parameter was provided or use the default
+    if [[ -z "$WORKSPACE" ]]; then
+        WORKSPACE="$DEF_WORKSPACE"
+    fi
+        
+    # Construct API Endpoint
+    endpoint="https://api.bitbucket.org/2.0/repositories/$WORKSPACE/$REPO"
+
+    # Confirm deletion before proceeding
+    echo "Are you sure you want to delete the repository '$REPO' (y/N)?"
+    read -r confirmation
+    
+    if [[ $confirmation =~ ^[Yy]$ ]]; then
+        # Delete the repository
+        response=$(curl -sSL -X DELETE "$endpoint" \
+        -H "$BITBUCKET_AUTH" )
+
+        # Check if there are errors
+        echo "$response" | jq -r '.error.message'
+    else
+        echo "Deletion cancelled."
+    fi
+    
+}
+
+
+delete_bitbucket_repo "$1" "$2"
