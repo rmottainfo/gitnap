5aa17ebda4304b21bb82bb82d039764484e820b2
rmotta.net:~# <114815819+rmottanet@users.noreply.github.com>
2024-12-15 16:58:02 -0300

    Feature: Search Repositories (#3)
    * Github search repo
    
    * Gitlab search repo
    
    ---------
    
    Co-authored-by: Rodrigo Motta <rmotta.info@gmail.com>

---
+++ src/github/gh-search-repo.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+
+function search_github_repo() {
+    local QUERY
+    local endpoint
+    local response
+    local tmp_file
+
+    # String to query
+    QUERY="$1"
+    
+    # Checks if the parameter was provided
+    if [[ -z "$QUERY" ]]; then
+        echo "A query to search is necessary"
+    fi
+
+    # Construct API Endpoint
+    endpoint="https://api.github.com/search/repositories?q=$QUERY"
+
+    response=$(curl -s "$endpoint" \
+        -H "Accept: application/vnd.github+json" \
+        -H "Authorization: Bearer $GITHUB_TOKEN" \
+        -H "X-GitHub-Api-Version: 2022-11-28" \
+        | jq -r '.items[] | "Name: \(.name), Description: \(.description), Web URL: \(.html_url)"' )
+    
+    # See in terminal
+    tmp_file=$(mktemp)
+    echo "$response" > "$tmp_file"
+    less "$tmp_file"
+    rm "$tmp_file"
+
+}
+
+search_github_repo "$1"
+
