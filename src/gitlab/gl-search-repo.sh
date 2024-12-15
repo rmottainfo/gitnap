e1330d53473b524600ba14e460f900e0fc9643d2
Rodrigo Motta <rmotta.net@gmail.com>
2024-12-15 14:06:44 -0300

    Gitlab search repo

---
+++ src/gitlab/gl-search-repo.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+
+function search_gitlab_repo() {
+    local QUERY
+    local endpoint
+    local response
+    local tmp_file
+
+    # String to query
+    QUERY="$1"
+    
+    # Checks if the parameter was provided
+    if [[ -z "$QUERY" ]]; then
+        echo "A query to search is necessary"
+    fi
+
+    # Construct API Endpoint
+    endpoint="https://gitlab.com/api/v4/search?scope=projects&search=$QUERY"
+
+    # CURL and jq
+    response=$(curl -s "$endpoint" \
+        -H "Content-Type: application/json" \
+        -H "Authorization: Bearer $GITLAB_TOKEN" \
+        | jq -r '.[] | "Name: \(.name), Description: \(.description), Web URL: \(.web_url)"' )
+
+    # See in terminal
+    tmp_file=$(mktemp)
+    echo "$response" > "$tmp_file"
+    less "$tmp_file"
+    rm "$tmp_file"
+
+}
+
+search_gitlab_repo "$1"
