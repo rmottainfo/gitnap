d03b39795cfd2132479a0cf8adb2bbb16d5edc90
rmotta.net:~# <114815819+rmottanet@users.noreply.github.com>
2025-03-18 16:49:51 -0300

    Feature: Pull Request Create (#4)
    * Bitbucket create pull request
    
    * Github create pull request
    
    * GitLab create pull request
    
    * Utils enhanced with of the following:
    
    commit 26666f06a0003cd0c7e65d33bdb52364ae3e404f
    Author: Rodrigo Motta <rmotta.info@gmail.com>
    Date:   Sun Mar 16 23:22:39 2025 -0300
    
        Endpoint builder utility
    
    commit b8467c5bda4babee6cb0aa53f9a919514877da37
    Author: Rodrigo Motta <rmotta.info@gmail.com>
    Date:   Sun Mar 16 23:19:43 2025 -0300
    
        Update default settings
    
    commit 1f73c99aeca01115c1e206c87da505b8ff2e9231
    Author: Rodrigo Motta <rmotta.info@gmail.com>
    Date:   Sun Mar 16 23:16:51 2025 -0300
    
        Format pull request utility
    
    commit 1346b5e84acb04946e7920c85e65f19fb7276713
    Author: Rodrigo Motta <rmotta.info@gmail.com>
    Date:   Sun Mar 16 23:11:46 2025 -0300
    
        Authentication utility
    
    ---------
    
    Co-authored-by: Rodrigo Motta <rmotta.info@gmail.com>

---
+++ src/gitlab/gl-pr-create.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+
+# Authentication and default settings
+source "$GITNAP/utils/auth.sh"
+source "$GITNAP/utils/endpoints.sh"
+source "$GITNAP/utils/settings.sh"
+source "$GITNAP/utils/format_pullrequest.sh"
+
+
+function create_gitlab_pullrequest() {
+    local REPO
+    local OWNER
+    local pr_title
+    local pr_body
+    local source_branch
+    local target_branch
+    local payload
+    local endpoint
+    local response
+        
+    # Name of repository
+    REPO="$1"
+    
+    # Verifica se o parâmetro foi fornecido
+    if [[ -z "$REPO" ]]; then
+        # Se não foi fornecido, utiliza o nome do diretório atual
+        REPO=$(basename "$PWD")
+    fi
+
+    # Repo owner
+    OWNER="$2"
+    
+    # Verifica se o parâmetro foi fornecido
+    if [[ -z "$OWNER" ]]; then
+        # Se não foi fornecido, utiliza o padrão definido em auth.sh
+        OWNER="$DEF_GL_OWNER"
+    fi
+        
+    pr_title="$(format_pullrequest "title")"
+    pr_body="$(format_pullrequest "body")"
+    
+    source_branch="$CURRENT_BRANCH"
+    target_branch="$DEFAULT_BRANCH"
+
+    # Create the JSON payload for the repository
+    payload='{"title":"'"$pr_title"'", "description":"'"$pr_body"'",
+        "source_branch":"'"$source_branch"'","target_branch":"'"$target_branch"'",
+        "remove_source_branch":"True", "squash":"True"}'
+
+    # Construct the endpoint URL
+    endpoint="$(build_gl_endpoint "PR" "$OWNER" "$REPO")"
+    
+    # Create the repository using curl
+    response=$(curl -sSL -X POST "$endpoint" \
+        -H "Content-Type: application/json" \
+        -H "Authorization: Bearer $GITLAB_TOKEN" \
+        -d "$payload" )
+
+    # Check if there are erros
+    echo "$response" | jq -r '.iid // .message'
+}
+
+create_gitlab_pullrequest "$1" "$2" 
+
---
+++ src/utils/format_pullrequest.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+
+function format_pullrequest() {
+  local content
+  local section="$1"  # Título ou corpo
+  local file="pr"     # Nome do arquivo
+
+  # Lê o arquivo
+  content=$(cat "$file")
+
+  # Extrai a seção desejada
+  if [[ "$section" == "title" ]]; then
+    content=$(head -n 1 "$file")
+  else
+    content=$(tail -n +2 "$file")
+  fi
+
+  # Escapa aspas duplas e quebras de linha
+  escaped_content=$(echo "$content" | sed "s/'/\\'/g; s/$/\\\n/g; s/\n//g;" | tr -d '\n')
+
+  # Retorna o conteúdo formatado
+  echo "$escaped_content"
+
+}
