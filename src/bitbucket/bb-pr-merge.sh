3064bbe4efaec69b0df7b70a6187f4d96e46a4b5
rmotta.net:~# <114815819+rmottanet@users.noreply.github.com>
2025-03-23 13:27:33 -0300

    Feature: Pull Request Merge (#5)
    * Bitbucket merge pull request
    
    * Github merge pull request
    
    * Gitlab merge pull request

---
+++ src/bitbucket/bb-pr-merge.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash
+
+# Authentication and default settings
+source "$GITNAP/utils/auth.sh"
+source "$GITNAP/utils/endpoints.sh"
+source "$GITNAP/utils/settings.sh"
+source "$GITNAP/utils/format_pullrequest.sh"
+
+
+function create_bitbucket_pullrequest() {
+    local pr
+    local REPO
+    local WORKSPACE
+    local payload
+    local pr_endpoint
+    local endpoint
+    local response
+    
+    # Pull Request number
+    pr="$1"
+
+    # Verifica se o parâmetro foi fornecido
+    if [[ -z "$pr" ]]; then
+        echo "Pull Request number is required."
+        exit 1
+    fi
+        
+    # Name of repository
+    REPO="$2"
+    
+    # Verifica se o parâmetro foi fornecido
+    if [[ -z "$REPO" ]]; then
+        # Se não foi fornecido, utiliza o nome do diretório atual
+        REPO=$(basename "$PWD")
+    fi
+
+    # Repo owner
+    WORKSPACE="$3"
+    
+    # Verifica se o parâmetro foi fornecido
+    if [[ -z "$WORKSPACE" ]]; then
+        # Se não foi fornecido, utiliza o padrão definido em auth.sh
+        WORKSPACE="$DEF_WORKSPACE"
+    fi
+        
+    # Create the JSON payload for the repository
+    payload='{
+        "type": "merge",
+        "message": "merging pullrequests",
+        "close_source_branch": true,
+        "merge_strategy": "squash"
+    }'
+
+    # Construct URL endpoint
+    pr_endpoint="$(build_bb_endpoint "PR" "$WORKSPACE" "$REPO")"
+    endpoint="$endpoint/$pr/merge"
+    
+    # Create the repository using curl
+    response=$(curl -sSL -X POST "$endpoint" \
+        -H "$BITBUCKET_AUTH" \
+        -H 'Accept: application/json' \
+        -H 'Content-Type: application/json' \
+        -d "$payload" )
+
+    # Check if there are erros
+    echo "$response" #| jq -r '.iid // .message'
+}
+
+create_bitbucket_pullrequest "$1" "$2" "$3" 
---
+++ src/github/gh-pr-merge.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+
+# Authentication and default settings
+source "$GITNAP/utils/auth.sh"
+source "$GITNAP/utils/endpoints.sh"
+source "$GITNAP/utils/settings.sh"
+source "$GITNAP/utils/format_pullrequest.sh"
+
+
+function merge_github_pullrequest() {
+    local pr
+    local REPO
+    local OWNER
+    local pr_title
+    local pr_body
+    local source_branch
+    local target_branch
+    local payload
+    local endpoint
+    local response
+        
+    # Pull Request number
+    pr="$1"
+
+    # Verifica se o parâmetro foi fornecido
+    if [[ -z "$pr" ]]; then
+        echo "Pull Request number is required."
+        exit 1
+    fi
+        
+    # Name of repository
+    REPO="$2"
+    
+    # Verifica se o parâmetro foi fornecido
+    if [[ -z "$REPO" ]]; then
+        # Se não foi fornecido, utiliza o nome do diretório atual
+        REPO=$(basename "$PWD")
+    fi
+
+    # Repo owner
+    OWNER="$3"
+    
+    # Verifica se o parâmetro foi fornecido
+    if [[ -z "$OWNER" ]]; then
+        # Se não foi fornecido, utiliza o padrão definido em auth.sh
+        OWNER="$DEF_GH_OWNER"
+    fi
+        
+    # Create the JSON payload for the repository
+    payload='{"merge_method":"squash"}'
+
+    # Construct URL endpoint
+    pr_endpoint="$(build_gh_endpoint "PR" "$OWNER" "$REPO")"
+    endpoint="$pr_endpoint/$pr/merge"
+    
+    # Create the repository using curl
+    response=$(curl -sSL -X PUT "$endpoint" \
+        -H "Accept: application/vnd.github+json" \
+        -H "Authorization: Bearer $GITHUB_TOKEN" \
+        -H "X-GitHub-Api-Version: 2022-11-28" \
+        -d "$payload" )
+
+    # Check if there are erros
+    echo "$response" #| jq -r '.number // .message'
+}
+
+merge_github_pullrequest "$1" "$2" "$3"
